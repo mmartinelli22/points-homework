03ed1af1cda7c7a2212ebcafaeed53b828ab4e9c
Michael Martinelli <turtelboy123@gmail.com>
2023-10-30 10:53:09 -0600

    clean up notes

--- src/App/apiCall.js
+++ src/App/apiCall.js
@@ -1,19 +1,5 @@
 import { loyaltyMembers } from './../_mock_/mockData'
 export const fetchData = async () => {
     const response = loyaltyMembers;
-    console.log(response, 'response')
-    // if (!response.ok) {
-    //     throw new Error('Sorry, no users found')
-    // }
-    // else {
-    // const data = await response.json().then(arg => {
-    //     console.log({
-    //         arg
-    //     });
-    // });
-    // console.log({
-    //     data
-    // })
     return response;
-    // }
 };
